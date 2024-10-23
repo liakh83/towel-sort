ca817c4c33309a268725bbe25acaf024724bcdf3
liakh83 <lahn4573@gmail.com>
2024-10-23 22:08:06 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    //  Check for empty array
+    if (!matrix || matrix.length === 0) return [];
+    // Go the array elements turn over the odd ones and make the array one-dimensional
+    return matrix.flatMap((row, index) =>
+        index % 2 === 0 ? row : row.reverse()
+    );
+};
